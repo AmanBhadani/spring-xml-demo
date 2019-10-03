9a7cb200598a0ddc3f08f4aedd5cc2e427736591
AmanBhadani <aman.bhadani.01@gmail.com>
2019-10-03 10:43:51 +0530

    first commit

---
+++ src/main/java/com/stackroute/App.java
@@ -0,0 +1,20 @@
+package com.stackroute;
+
+import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie = context.getBean("movie", Movie.class);
+        System.out.println(movie.toString());
+
+    }
+}
